630faeff963cd14fea5ab6a835fb924b1f50a92e
Samuel Giks <samuelgiks254@gmail.com>
2026-07-22 01:16:19 +0300

    adding static styling

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(is_published=True)
-    
+    posts_qs = Post.objects.filter(is_published=True).order_by('-published_at')
+    paginator = Paginator(posts_qs, 5) # Show 5 posts per page
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
     return render(request, 'blog/post_list.html', {'posts':posts})
   
 def post_detail(request, slug):
