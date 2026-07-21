8493655c61ca39738b7648162455eec7e9496326
Samuel Giks <samuelgiks254@gmail.com>
2026-07-22 00:07:16 +0300

    adding base,post_detail and post_list templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post 
 
-# Register your models here.
+# Register your model
+@admin.register(Post)
+class Postadmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'updated_at', 'published_at', 'is_published')
+    list_filter = ('is_published', 'author', 'created_at')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}#tells the admin: "auto-generate the slug field's value from the title field, live, as the user types."
+    date_hierarchy = 'published_at' #adds a drill-down date navigation bar at the top of the admin's post list — showing years, then clicking a year shows months, then days.
+    ordering = ('-published_at',) #The - prefix means descending order. So -published_at = newest posts first. Without the -, it'd sort oldest first.
--- blog/views.py
+++ blog/views.py
@@ -9,4 +9,6 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts':posts})
   
 def post_detail(request, slug):
-  post = 
+  post = get_object_or_404(Post, slug=slug, is_published=True)
+  
+  return render(request, 'blog/post_detail.html', {'post': post})
