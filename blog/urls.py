a674e0eb59be46b7ef8ba65a97a972eff2ee2f2c
Samuel Giks <samuelgiks254@gmail.com>
2026-08-19 17:25:26 +0300

    enable editing

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.post_list, name='post_list'),
   path('new/', views.post_create, name='post_create'),
+  path('<slug:slug>/edit/', views.post_edit, name='post_edit'),
   path('<slug:slug>/', views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm
+from django.core.exceptions import PermissionDenied
+
 
 # Create your views here.
 def post_list(request):
@@ -33,3 +35,27 @@ def post_create(request):
         form = PostForm()
         
     return render(request, 'blog/post_form.html', {'form': form})
+
+
+
+@login_required
+def post_edit(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    
+    if post.author != request.user:
+        raise PermissionDenied("You are not allowed to edit this post ")
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('post_detail', slug=post.slug)
+        
+    else:
+        form = PostForm(instance=post)
+        
+        
+    return render(request, 'blog/post_form.html', {'form': form, 'editing': True})
+            
